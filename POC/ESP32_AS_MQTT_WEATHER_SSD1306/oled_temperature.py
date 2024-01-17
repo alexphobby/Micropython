f0792bd4a5afef9ca3e557e81dc28e958fe31653
alexphobby <109878425+alexphobby@users.noreply.github.com>
2024-01-17 15:46:22 +0200

    upd

--- POC/ESP32_AS_MQTT_WEATHER_SSD1306/oled_temperature.py
+++ POC/ESP32_AS_MQTT_WEATHER_SSD1306/oled_temperature.py
@@ -64,7 +64,7 @@ oled.show()
 async def mqtt_send_temp(client):
     while True:
         try:
-            print(f"Send mqtt message on {machines.topic_send}")
+            print(f"Send mqtt message on {my_machine.topic_send}")
             output = {"devicename":str(my_machine.device),"roomname":str(my_machine.name),"ip": str(client.ip), "temperature":str(hdc1080.temperature()),"humidity":str(hdc1080.humidity()),"ambient":str(0),"dim":str(0),"lastmotion":0,"autobrightness":0}
             await client.publish(my_machine.topic_send, f'jsonDiscovery:{output}', qos = 0)
             await asyncio.sleep(10)
