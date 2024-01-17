bd7b1daf8bf6e8d84b5abd746312ab395af9e6b0
alexphobby <109878425+alexphobby@users.noreply.github.com>
2024-01-17 12:08:18 +0200

    update senko

--- POC/ESP32_AS_MQTT_WEATHER_SSD1306/senko.py
+++ POC/ESP32_AS_MQTT_WEATHER_SSD1306/senko.py
@@ -1,12 +1,22 @@
-import urequests
+import requests
 import uhashlib
-
+import gc
 
 class Senko:
     raw = "http://raw.githubusercontent.com"
     github = "http://github.com"
+    headers = {}
 
-    def __init__(self, user, repo, url=None, branch="master", working_dir="app", files=["boot.py", "main.py"], headers={}):
+    def __init__(self, user, repo, url=None, branch="master", working_dir="app", files=["boot.py", "main.py"] , all_folder=False, headers={}):
+        self.files = files
+        
+        self.all_folder = all_folder
+        if len(headers) ==0:
+            self.headers = {"User-Agent":user}
+        else:
+            self.headers = headers
+            
+        print(f"headers: {self.headers}")
         """Senko OTA agent class.
 
         Args:
@@ -20,8 +30,15 @@ class Senko:
         """
         self.base_url = "{}/{}/{}".format(self.raw, user, repo) if user else url.replace(self.github, self.raw)
         self.url = url if url is not None else "{}/{}/{}".format(self.base_url, branch, working_dir)
-        self.headers = headers
-        self.files = files
+        #self.headers = headers
+        
+        if "*" in files:
+            print("Download all folder")
+            self.all_folder = True
+            self.files = []
+        else:
+            print("Download specific files")
+            self.files = files
 
     def _check_hash(self, x, y):
         x_hash = uhashlib.sha1(x.encode())
@@ -36,8 +53,8 @@ class Senko:
             return False
 
     def _get_file(self, url):
-        print(f"url: {url}")
-        payload = urequests.get(url, headers=self.headers)
+        #print(f"url: {url}")
+        payload = requests.get(url, headers=self.headers)
         code = payload.status_code
 
         if code == 200:
@@ -47,8 +64,18 @@ class Senko:
 
     def _check_all(self):
         changes = []
+        
+        if self.all_folder:
+            print(f"headers: {self.headers}")
+            res = requests.get("http://api.github.com/repos/alexphobby/Micropython/contents/POC/ESP32_AS_MQTT_WEATHER_SSD1306",headers = self.headers).json()
+            for obj in res:
+                print(f"Found file {obj['name']} in repo")
+                self.files.append(obj['name'])
+            res =""
+            gc.collect()
 
         for file in self.files:
+            print(f"Local check file: {file}")
             latest_version = self._get_file(self.url + "/" + file)
             if latest_version is None:
                 continue
@@ -61,6 +88,7 @@ class Senko:
 
             if not self._check_hash(latest_version, local_version):
                 changes.append(file)
+            gc.collect()
 
         return changes
 
@@ -85,7 +113,9 @@ class Senko:
 
         for file in changes:
             with open(file, "w") as local_file:
+                print(f"Write file {file}")
                 local_file.write(self._get_file(self.url + "/" + file))
+                gc.collect()
 
         if changes:
             return True
--- POC/ESP32_AS_MQTT_WEATHER_SSD1306/senko_test.py
+++ POC/ESP32_AS_MQTT_WEATHER_SSD1306/senko_test.py
@@ -1,11 +1,11 @@
-from i2c_init import *
-
-
-from mqtt_as_init import *
-test()
+#from i2c_init import *
+#from mqtt_as_init import *
+#test()
+from CONNECTWIFI import *
+wifi = CONNECTWIFI()
 
 from senko import Senko
-OTA = Senko(user="alexphobby",branch = "main", repo="Micropython", working_dir="POC/ESP32_AS_MQTT_WEATHER_SSD1306", files=["test.py"])
+OTA = Senko(user="alexphobby",branch = "main", repo="Micropython", working_dir="POC/ESP32_AS_MQTT_WEATHER_SSD1306", files=["*"])
 OTA.update()
 
 if OTA.update():
