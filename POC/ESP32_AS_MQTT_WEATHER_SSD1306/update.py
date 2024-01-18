71170073129133bf17b9d326b3ff6a2f1af1ba6d
alexphobby <109878425+alexphobby@users.noreply.github.com>
2024-01-18 09:39:36 +0200

    update

---
+++ POC/ESP32_AS_MQTT_WEATHER_SSD1306/update.py
@@ -0,0 +1,18 @@
+#from i2c_init import *
+#from mqtt_as_init import *
+#test()
+
+def update():
+    import gc
+    import CONNECTWIFI
+    wifi = CONNECTWIFI.CONNECTWIFI()
+    import MACHINES
+    my_machine = MACHINES.MACHINES()
+
+    from senko import Senko
+    gc.collect()
+    OTA = Senko(user="alexphobby",branch = "main", repo="Micropython", working_dir=my_machine.github_folder, files=["*"])
+    result = OTA.update()
+    if result:
+        print("Updated to the latest version! Rebooting...")
+        machine.reset()
