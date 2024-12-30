cf977bad5fc4b29ee9b374b50c91af0434298638
alexphobby <109878425+alexphobby@users.noreply.github.com>
2024-12-30 14:06:23 +0200

    u

--- POC/ESP32_S3_BOILER_CONTROL/update.py
+++ POC/ESP32_S3_BOILER_CONTROL/update.py
@@ -1,15 +1,16 @@
 #from i2c_init import *
 #from mqtt_as_init import *
 #test()
-
+print("Update from GitHub. Usage: update()")
 def update():
-    import gc
-    import CONNECTWIFI
-    wifi = CONNECTWIFI.CONNECTWIFI()
     import MACHINES
     my_machine = MACHINES.MACHINES()
+    import gc
     import machine
-
+    
+    import CONNECTWIFI
+    wifi = CONNECTWIFI.CONNECTWIFI()
+    
     from senko import Senko
     gc.collect()
     OTA = Senko(user="alexphobby",branch = "main", repo="Micropython", working_dir=my_machine.github_folder, files=["*"])
